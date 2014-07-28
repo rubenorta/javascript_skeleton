3182c692aacc0a3d86bad3b3fe0d5121af767136
Ruben Orta <ruben.orta@gmail.com>
2014-07-28 11:01:05 +0200

    Init project

---
+++ app/class_under_test.js
@@ -0,0 +1,3 @@
+var class_under_test = (function(cards) {
+  return class_under_test;
+})(class_under_test || {});
---
+++ test/poker.spec.js
@@ -0,0 +1,7 @@
+describe('kata', function() {
+
+  it('should be an object', function() {
+    expect(class_under_test).to.be.an('object');
+  });
+
+});
